f1f3ab3a061bb6a2b01ad696b67e31e9d2e8cdcb
HunterXVD11 <81260156+HunterXVD11@users.noreply.github.com>
2022-12-18 17:44:45 -0300

    Atualizando pubspec.yaml

---
+++ loja_anuncios/lib/pages/page_anuncios_main.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import "dart:async";
+import "dart:convert";
+import 'package:flutter/material.dart';
+import 'package:loja_anuncios/model/user.dart';
+import 'package:loja_anuncios/model/anuncio.dart';
+import 'package:loja_anuncios/pages/cadastro_user.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import '../CRUD/crud_user.dart';
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
